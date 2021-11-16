00ee8e99b8f954ebe73e6fc6aca1b2cbe8469db6
Sazid <sazid.sunny@gmail.com>
2021-11-16 16:12:37 +0530

    Oranges added

--- rz-vs2019-GitDemo111/Program.cs
+++ rz-vs2019-GitDemo111/Program.cs
@@ -7,6 +7,7 @@ namespace rz_vs2019_GitDemo111
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Oranges");
         }
     }
 }
