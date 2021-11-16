ce53a77caa7c246927e7718f4a2cf3449e0bb7cf
Nazeeruddin <naseers.tech@gmail.com>
2021-11-16 16:04:52 +0530

    Add project files.

---
+++ rz-vs2019-GitDemo111/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rz_vs2019_GitDemo111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
